f1de57a3c94cfd6d99a79f492555ccb851e1862f
talhaanwarstudent <talhaanwarupwork@gmail.com>
2022-07-12 12:12:58 +0500

    mvc implimented

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import { AppModule } from './modules/app/app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import {resolve } from 'path';
 require('dotenv').config()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -23,6 +26,9 @@ async function bootstrap() {
   }))
   app.use(passport.initialize())
   app.use(passport.session())
+  app.useStaticAssets(join(resolve('./src/public')));
+  app.setBaseViewsDir(join(resolve('./src/resources/views')));
+  app.setViewEngine('hbs');
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
